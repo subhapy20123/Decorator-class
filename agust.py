8660b927cfee1bfb2177332dcd84ed38e21139e3
unknown <subiendumaj@gmail.com>
2024-08-11 14:22:27 -0500

    i m good by

--- agust.py
+++ agust.py
@@ -1,10 +1,14 @@
 list_name=["rajuz","somnath","biswnath","subho"]
+
 unique_name=list(set(list_name))
+
 print(unique_name)
 result_map=map(lambda x:x.lower(),list_name)
 print("result_map",list(result_map))
 lower_name=list(result_map)
+
 return_sort=sorted(lower_name,reverse=True)
 lower_name.sort(reverse=True)
 print("ruselt_map sort",lower_name)
+
 print("return_sort",return_sort)
